89b788c876b8a9fe9fb82ce7bd93fcc04a19782d
francis-pouatcha <francis.pouatcha@adorsys.com>
2015-07-14 01:37:45 +0200

    stabilizing the container

---
+++ adcom/adcatal.server/src/test/java/org/adorsys/adcatal/rest/integ/CatalArticleEndpointIT.java
@@ -0,0 +1,71 @@
+package org.adorsys.adcatal.rest.integ;
+
+import java.math.BigDecimal;
+
+import org.adorsys.adcatal.jpa.CatalArticle;
+import org.adorsys.adcatal.jpa.CatalArticleSearchInput;
+import org.adorsys.adcatal.rest.integ.utils.RestAssuredITConfig;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class CatalArticleEndpointIT extends RestAssuredITConfig {
+
+	private static final String PATH = "/catalarticles";
+	@Test
+	public void testCreateFind() {
+		CatalArticle catalArticle = createCatalArticle();
+		Assert.assertTrue("New Object must have empty id", org.apache.commons.lang3.StringUtils.isBlank(catalArticle.getId()));
+		CatalArticle catalArticle2 = RestAssured.expect().statusCode(200).when().given().contentType(ContentType.JSON).body(catalArticle).post(PATH).as(CatalArticle.class);
+		Assert.assertNotNull(catalArticle2);
+		Assert.assertTrue("Created Object must have non empty id", org.apache.commons.lang3.StringUtils.isNotBlank(catalArticle2.getId()));
+		Assert.assertTrue("Created Object must be equals", catalArticle.contentEquals(catalArticle2));
+		
+		String identif = catalArticle2.getIdentif();
+		
+		CatalArticle catalArticle3 = RestAssured.get(PATH + "/"+ identif).as(CatalArticle.class);
+		Assert.assertTrue("Created Object must be equals", catalArticle.contentEquals(catalArticle3));
+		
+	}
+
+	@Test
+	public void testCreateFindByIdentifFromTo() {
+		CatalArticle catalArticle = createCatalArticle();
+		Assert.assertTrue("New Object must have empty id", org.apache.commons.lang3.StringUtils.isBlank(catalArticle.getId()));
+		CatalArticle catalArticle2 = RestAssured.expect().statusCode(200).when().given().contentType(ContentType.JSON).body(catalArticle).post(PATH).as(CatalArticle.class);
+		Assert.assertNotNull(catalArticle2);
+		Assert.assertTrue("Created Object must have non empty id", org.apache.commons.lang3.StringUtils.isNotBlank(catalArticle2.getId()));
+		Assert.assertTrue("Created Object must be equals", catalArticle.contentEquals(catalArticle2));
+		
+		String identif = catalArticle2.getIdentif();
+		
+		CatalArticleSearchInput catalArticleSearchInput = new CatalArticleSearchInput();
+		catalArticleSearchInput.setIdentifFrom(identif);
+		catalArticleSearchInput.setIdentifTo(identif);
+		CatalArticle catalArticle3 = RestAssured.get(PATH + "/"+ identif).as(CatalArticle.class);
+		Assert.assertTrue("Created Object must be equals", catalArticle.contentEquals(catalArticle3));
+		
+	}
+	
+	private CatalArticle createCatalArticle(){
+		CatalArticle catalArticle = new CatalArticle();
+
+		catalArticle.setIdentif(RandomStringUtils.randomAlphanumeric(7).toUpperCase());
+		catalArticle.setActive(true);
+		catalArticle.setAuthorizedSale(true);
+		catalArticle.setSppu(new BigDecimal(RandomStringUtils.randomNumeric(4)));
+		catalArticle.setSppuCurrIso3("XAF");
+		catalArticle.setMaxDisctRate(new BigDecimal(RandomStringUtils.randomNumeric(1)));
+		catalArticle.setMaxStockQty(new BigDecimal(RandomStringUtils.randomNumeric(4)));
+		catalArticle.setMinStockQty(new BigDecimal(RandomStringUtils.randomNumeric(2)));
+		catalArticle.setVatRate(new BigDecimal("19.25"));
+		// Defaults to FiFo
+		catalArticle.setLotMgtScheme("FiFo");
+		catalArticle.setMngByLot(true);		
+		
+		return catalArticle;
+	}
+}
---
+++ adcom/adcatal.server/src/test/java/org/adorsys/adcatal/rest/integ/utils/RestAssuredITConfig.java
@@ -0,0 +1,50 @@
+package org.adorsys.adcatal.rest.integ.utils;
+
+import java.util.logging.Logger;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.BeforeClass;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.DecoderConfig;
+import com.jayway.restassured.config.EncoderConfig;
+import com.jayway.restassured.config.ObjectMapperConfig;
+import com.jayway.restassured.config.RestAssuredConfig;
+import com.jayway.restassured.mapper.factory.Jackson2ObjectMapperFactory;
+
+/**
+ * Base Class for RestAssured Tests
+ * @author fhi@adorsys.de
+ */
+public class RestAssuredITConfig {
+	
+	@BeforeClass
+	public static void init() {
+		String host = System.getProperty("host");
+		String port = System.getProperty("port");
+		
+		RestAssured.baseURI = String.format("http://%s", StringUtils.isBlank(host) ? "localhost" : host);
+		RestAssured.port = StringUtils.isBlank(port) ? 8080 : Integer.valueOf(port);
+		RestAssured.basePath = "/adcatal.server/rest";
+		
+		// register ObjectId Serializer
+		RestAssured.config = RestAssuredConfig.config().objectMapperConfig(new ObjectMapperConfig().jackson2ObjectMapperFactory(
+		new Jackson2ObjectMapperFactory() {
+		        @Override
+		        @SuppressWarnings("rawtypes")
+		        public ObjectMapper create(Class clazz, String charset) {
+		            ObjectMapper objectMapper = new ObjectMapper();
+		            // CUstomize object mapper
+		            return objectMapper;
+		        }
+		    }
+		))
+		// and force UTF-8
+		.encoderConfig(EncoderConfig.encoderConfig().defaultContentCharset("UTF-8"))
+		.decoderConfig(DecoderConfig.decoderConfig().defaultContentCharset("UTF-8"));
+		
+		Logger.getLogger(RestAssuredITConfig.class.getName()).info("Initialized RestAssured for "+RestAssured.baseURI+":"+RestAssured.port+RestAssured.basePath);
+	}
+	
+}
