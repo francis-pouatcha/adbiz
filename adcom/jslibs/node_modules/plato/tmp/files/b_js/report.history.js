baab5ada1f97485c88c45fadb1da0ffba5f0b764
francis-pouatcha <francis.pouatcha@adorsys.com>
2015-08-15 09:40:01 -0400

    reorganizing bower files

---
+++ adcom/jslibs/node_modules/plato/tmp/files/b_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Sat, 09 May 2015 17:02:55 GMT","sloc":14,"lloc":8,"functions":2,"deliveredBugs":0.052,"maintainability":97.80277542266379,"lintErrors":[],"difficulty":8.166666666666668}]
