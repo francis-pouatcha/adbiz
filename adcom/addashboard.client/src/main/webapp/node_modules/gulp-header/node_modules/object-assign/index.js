0ff29af35912c256d7cab8a924e06c2af14dd51c
guymoyo <guymoyo@yahoo.fr>
2015-08-21 11:06:20 +0100

    fix

---
+++ adcom/addashboard.client/src/main/webapp/node_modules/gulp-header/node_modules/object-assign/index.js
@@ -0,0 +1,36 @@
+'use strict';
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function ToObject(val) {
+	if (val == null) {
+		throw new TypeError('Object.assign cannot be called with null or undefined');
+	}
+
+	return Object(val);
+}
+
+module.exports = Object.assign || function (target, source) {
+	var from;
+	var keys;
+	var to = ToObject(target);
+	var symbols;
+
+	for (var s = 1; s < arguments.length; s++) {
+		from = Object(arguments[s]);
+
+		for (var key in from) {
+			if (hasOwnProperty.call(from, key)) {
+				to[key] = from[key];
+			}
+		}
+
+		if (Object.getOwnPropertySymbols) {
+			symbols = Object.getOwnPropertySymbols(from);
+			for (var i = 0; i < symbols.length; i++) {
+				to[symbols[i]] = from[symbols[i]];
+			}
+		}
+	}
+
+	return to;
+};
