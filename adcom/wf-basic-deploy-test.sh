6ecb4cd12585d0ff9fa885bb06b560a9595b0985
francis-pouatcha <francis.pouatcha@adorsys.com>
2015-09-21 01:40:31 +0200

    finish of keycloack configuration

--- adcom/wf-basic-deploy-test.sh
+++ adcom/wf-basic-deploy-test.sh
@@ -167,6 +167,7 @@ rm $JBOSS_DEPLOY_DIR/adkcloak.config.war
 
 # deploy modules
 echo "             deploying new artifacts"
+cp $ADCOM_HOME/addashboard.client/target/addashboard.client.war $JBOSS_HOME/standalone/deployments/
 cp $ADCOM_HOME/adcatal.server/target/adcatal.server.war $JBOSS_HOME/standalone/deployments/
 cp $ADCOM_HOME/adcatal.client/target/adcatal.client.war $JBOSS_HOME/standalone/deployments/
 
