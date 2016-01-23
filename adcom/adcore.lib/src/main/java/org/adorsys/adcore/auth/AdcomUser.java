bcd329c580751c490de4f97a0d3cada2d032db96
fpo <fpo@adorsys.com>
2016-01-23 03:16:22 +0100

    Bugfixes adinvt clients

--- adcom/adcore.lib/src/main/java/org/adorsys/adcore/auth/AdcomUser.java
+++ adcom/adcore.lib/src/main/java/org/adorsys/adcore/auth/AdcomUser.java
@@ -1,11 +1,24 @@
 package org.adorsys.adcore.auth;
 
+import org.keycloak.KeycloakSecurityContext;
+
 public final class AdcomUser {
 
-	private String name;
+	private final String name;
 
+	private final KeycloakSecurityContext context;
+	
 	public AdcomUser(String name) {
+		this(name, null);
+	}
+
+	public AdcomUser(KeycloakSecurityContext context) {
+		this(context.getToken().getPreferredUsername(), context);
+	}
+
+	public AdcomUser(String name, KeycloakSecurityContext context) {
 		this.name = name;
+		this.context = context;
 	}
 
 	public String getLoginName(){
@@ -15,5 +28,8 @@ public final class AdcomUser {
 	public String getWorkspaceId(){
 		return name;
 	}
-	
+
+	public KeycloakSecurityContext getContext() {
+		return context;
+	}
 }
--- adcom/adcore.lib/src/main/java/org/adorsys/adcore/auth/AdcomUserProducer.java
+++ adcom/adcore.lib/src/main/java/org/adorsys/adcore/auth/AdcomUserProducer.java
@@ -7,6 +7,8 @@ import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Produces;
 
+import org.keycloak.KeycloakPrincipal;
+
 @Stateless
 public class AdcomUserProducer {
 
@@ -14,15 +16,20 @@ public class AdcomUserProducer {
 	private SessionContext sessionContext;
 	
 	private AdcomUser unauthUser = new AdcomUser("nobody");
-
+	
 	@Produces
 	public AdcomUser getCallerPrincipal() {
 		Principal callerPrincipal = sessionContext.getCallerPrincipal();
 		if(callerPrincipal==null){
 			return unauthUser;
 		}
-		String name = callerPrincipal.getName();
-		return new AdcomUser(name);
+		
+		if(callerPrincipal instanceof KeycloakPrincipal){
+			KeycloakPrincipal<?> kcPrincipal = (KeycloakPrincipal<?>) callerPrincipal;
+			return new AdcomUser(kcPrincipal.getKeycloakSecurityContext());
+		}
+
+		return new AdcomUser(callerPrincipal.getName());
 	}
 
 }
